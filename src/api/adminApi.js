e4b27dd4f0e9bbcdeca010038b51db9ed2b22662
zhangdayeb <you@example.com>
2025-07-21 08:05:51 +0800

    'up'

--- src/api/adminApi.js
+++ src/api/adminApi.js
@@ -409,3 +409,13 @@ export function noticeListDelApi(params) {
     return res
   })
 }
+
+// 集团信息获取
+export function setGroupApi(params) {
+  return axios.post({
+    url: '/set/group',
+    data: params
+  }).then((res) => {
+    return res
+  })
+}
